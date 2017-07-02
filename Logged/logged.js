e8cdd88b8b2b91ef2c5f51a9540ddd9eed8619ad
Alex Ristinmaa <onlyhere4thefun@gmail.com>
2017-07-02 20:43:58 +0200

    Adding Logged In Page

---
+++ Logged/logged.js
@@ -0,0 +1,28 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPvpAQt28MDmavbc31JUdurX1OIWM3wZQ",
+    authDomain: "onlyhere4thefun-7c384.firebaseapp.com",
+    databaseURL: "https://onlyhere4thefun-7c384.firebaseio.com",
+    projectId: "onlyhere4thefun-7c384",
+    storageBucket: "onlyhere4thefun-7c384.appspot.com",
+    messagingSenderId: "872492202975"
+  };
+  
+  firebase.initializeApp(config);
+
+  var successMessage = document.getElementById('Success');
+
+  firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+      successMessage.innerHTML = 'Succesfully logged in as ' + firebaseUser.email;
+
+    } else {
+      window.location = '/Login';
+    }
+  })
+
+
+
+    function getElem(elemID) {
+    return document.getElementById(elemID);
+  }
