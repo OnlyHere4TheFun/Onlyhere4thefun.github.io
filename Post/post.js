4bab11ebd80fd255670c41ab7779162e2f34771e
OnlyHere4TheFun <onlyhere4thefun@gmail.com>
2017-07-08 00:22:37 +0200

    Update post.js

--- Post/post.js
+++ Post/post.js
@@ -1,13 +1,4 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBPvpAQt28MDmavbc31JUdurX1OIWM3wZQ",
-    authDomain: "onlyhere4thefun-7c384.firebaseapp.com",
-    databaseURL: "https://onlyhere4thefun-7c384.firebaseio.com",
-    projectId: "onlyhere4thefun-7c384",
-    storageBucket: "onlyhere4thefun-7c384.appspot.com",
-    messagingSenderId: "872492202975"
-  };
-  firebase.initializeApp(config);
+
 
   // Get Elements From The DOM
   var txtTitle = getElem('titleBox');
